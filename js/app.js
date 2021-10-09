e4f8d57e9e753f90e91c12b872fe7b3df0d2fde3
vattgern <sashawot060@gmail.com>
2021-10-09 22:07:39 +0400

    Done migth

--- js/app.js
+++ js/app.js
@@ -1,4 +1,74 @@
-const auth = Vue.createApp({
+const auth = Vue.createApp({});
+auth.component('personal-blog',{
+    template: `
+    <div class="auth__form">
+                <section>
+                    <div id="forNum">
+                        <label for="number">Телефон:</label>
+                        <input type="text" name="number" id="number" placeholder="Введите телефон"
+                            v-model="number">
+                        <!-- ! Проверка на телефон -->
+                        <small id="numCheck" :class="{ doneAll : isRightNum }">
+                            <h2>
+                                Телефон должен быть:
+                            </h2>
+                            <ul>
+                                <li :class="{ done : isAllNumber }">
+                                    Должен содержть только цифры
+                                </li>
+                                <li :class="{ done : isHaveLength }">
+                                    Должен содержть 11 символов
+                                </li>
+                            </ul>
+                        </small>
+                    </div>
+                    <div id="forPass">
+                        <label for="pass">Пароль:</label>
+                        <input type="password" name="pass" id="pass" placeholder="Введите пароль"
+                        v-model="password">
+                        <!-- ! Проверка пароля -->
+                        <small id="passCheck" :class="{ doneAll : isAllTrue }">
+                            <h2>
+                                Пароль должен быть:
+                            </h2>
+                            <ul>
+                                <li>
+                                    <span :class="{ done : isMinLength }">Минимум 6 символов</span>,
+                                </li>
+                                <li>
+                                    <span :class="{ done : isHaveNumber }">Содержать цифру(-ы)</span>
+                                </li>
+                                <li>
+                                    <span :class="{ done : isHaveSpecial }">Содержать спец. символ(-ы)</span>
+                                </li>
+                                <li>
+                                    <span :class="{ done : isMaxLength }">
+                                        Максимум 30 символов
+                                    </span>
+                                </li>
+                            </ul>
+                        </small>
+                    </div>
+                    <button class="submit__btn" @click="checkForm">
+                        Войти
+                    </button>
+                    <div :class = "{message}">
+                        <strong :class="{ doneAll : this.message.notFound }">
+                            Неправильно введены данные
+                        </strong>
+                        <strong :class="{ doneAll : this.message.backTo }">
+                            Неверный пароль
+                        </strong>
+                    </div>
+                </section>
+            </div>
+            <div v-if="isLogin" class="personal__blog">
+                <h1>Вы попали за защищенную страницу</h1>
+                <button @click="isLogin = false">
+                    Выход
+                </button>
+            </div>
+    `,
     data() {
         return {
             dataBase: {
@@ -11,7 +81,8 @@ const auth = Vue.createApp({
                 notFound: true,
                 // * Вывод окна о пароле и телефоне
                 backTo: true,
-                'messageBox': true
+                'messageBox': true,
+                notUser: true
             },
             password: '',
             isMinLength: false,
@@ -57,31 +128,17 @@ const auth = Vue.createApp({
                 } else{
                     this.password = '';
                     this.number = '';
-                    this.message.notFound = false;
+                    this.message.backTo = false;
                 }
             } else{
                 this.password = '';
                 this.number = '';
-                this.backTo = false;
+                this.message.notFound = false;
+                setTimeout(() => {
+                    this.message.notFound = true;
+                }, 4000);
             }
         },
-        exit(){
-            this.isLogin = false;
-        }
-    },
-}).component('personal-blog',{
-    methods: {
-        dead(){
-            let block = document.querySelector(".personal__blog").remove();
-        }
-    },
-    template: `
-        <div class="personal__blog">
-            <h1>Вы попали за защищенную страницу</h1>
-            <button @click="dead">
-                Выход
-            </button>
-        </div>
-    `
+    }
 });
 auth.mount("#auth");
